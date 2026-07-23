eb3332cd71ea975bab8b501ee666915a843f2764
robloxscripts2026-star <robloxscripts2026@gmail.com>
2026-07-23 02:18:54 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-getgenv().SCRIPT_KEY = "39d78cca-6be0-491e-b238-44a82d7fde61"
+getgenv().SCRIPT_KEY = "95491c1c-03df-48eb-9c0a-aa0d16e9b8ab"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/0de37ff1b934bd32bdd90c7bca39d1330164b47cfe52f98a5dca302e5c5668cd/download"))()
